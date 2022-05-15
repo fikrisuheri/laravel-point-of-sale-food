426f759adbc4e4c448b4ff09fdd132e7818780a4
Fikri <you@example.com>
2022-05-15 20:47:36 +0700

    installing inertia

---
+++ resources/js/Layouts/Guest.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div>
+           <h1>Selamat Datang</h1>
+        </div>
+            <slot />
+    </div>
+</template>
+
+<script>
+import { Link } from '@inertiajs/inertia-vue3';
+export default {
+    components: {
+        Link,
+    }
+}
+</script>
---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,13 @@
+<template>
+      <h1>LOGIN</h1>
+</template>
+
+<script>
+  import Guest from '../Layouts/Guest.vue';
+  import Authenticated from '../Layouts/Authenticated.vue';
+  import Button from '../Components/Button.vue';
+  export default {
+    layout:Authenticated,
+    components:Button
+  }
+</script>
